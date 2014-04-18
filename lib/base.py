61b06610395f992c5892c42371ae40795a07439b
Sam Kottler <shk@linux.com>
2014-04-18 10:56:39 +0600

    Add docstring for the appropriate classes and methods

--- lib/base.py
+++ lib/base.py
@@ -9,12 +9,20 @@ from sqlalchemy import create_engine
 from utils.database import Database
 
 class BaseHandler(tornado.web.RequestHandler):
+    """ This class is the base for all the other handlers. You should always
+    inherit from this class when initiating a class which is a handler to which
+    the application will direct requests. """
+
     @property
     def db(self):
-      config = self.load_db_config()
-      return create_engine("postgresql://%s@%s/%s" %
-          (config['username'], config['host'], config['database']))
+        """ Calls load_db_config() to fetch the credentials from the yaml
+        configuration and returns an Engine object. """
+        config = self.load_db_config()
+        return create_engine("postgresql://%s@%s/%s" %
+            (config['username'], config['host'], config['database']))
 
     @property
     def load_db_config(self):
+        """ Call the Database class to load and parse the configuration from
+        yml on disk. Returns a dict. """
         return Database().load_config()
--- utils/database.py
+++ utils/database.py
@@ -3,7 +3,10 @@ import os
 import yaml
 
 class Database(object):
+    """ Generic class for handling database-related tasks. """
     def load_config(self):
+        """ Pull the configuration off disk and parse the yml. Returns a dict
+        containing a subset of the yml from the 'database' section. """
         try:
             config = open("%s/config/cmdb.yml" %
                 os.path.join(os.path.dirname(__file__), '..'))
