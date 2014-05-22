1f9a979cc281e477c43bd27217ab23e46319d0db
Sam Kottler <shk@linux.com>
2014-05-22 15:19:39 +0600

    Add initial migration framework using existing DB connection code

--- utils/database.py
+++ utils/database.py
@@ -4,13 +4,25 @@ import yaml
 
 class Database(object):
     """ Generic class for handling database-related tasks. """
+
+    def select_config_for_environment(self):
+	""" Read the value of env variable and load the property
+	database configuration based on the value of DB_ENV.
+
+	Takes no input and returns a string like 'production' or 'testing'."""
+	return os.getenv('DB_ENV', 'development')
+
     def load_config(self):
         """ Pull the configuration off disk and parse the yml. Returns a dict
         containing a subset of the yml from the 'database' section. """
+	environment = self.select_config_for_environment()
+	config_src = ("%s/config/cmdb.%s.yml" % (
+	    (os.path.join(os.path.dirname(__file__), '..')),
+	    environment))
+	config = open(config_src)
+
         try:
-            config = open("%s/config/cmdb.yml" %
-                os.path.join(os.path.dirname(__file__), '..'))
-            return yaml.load(config)['database']
-        except LoadError:
-            print "Ensure config/cmdb.yml exists"
+	   return yaml.load(config)['database']
+	except:
+	    print ("Ensure config/cmdb.%s.yml exists" % config_src)
             return None
